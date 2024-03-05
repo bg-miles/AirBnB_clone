9445a473cf020ad42974e50f2e22630bb1c6bca1
bg-miles <piuschukwuebuka40@gmail>
2024-03-05 21:15:15 +0100

    a little update

--- models/base_model.py
+++ models/base_model.py
@@ -1,26 +1,21 @@
 import uuid
 import datetime
-class BaseModel:
-    def __init__(self, *args, **kwargs):
-        if len(kwargs) > 0:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ('created_at', 'updated_at'):
-                        setattr(self, key, datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                    else:
-                        setattr(self, key, value)
 
-        else: 
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.today().strftime('%Y-%m-%dT%H:%M:%S.%f')
+class BaseModel:
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
 
     def save(self):
-        self.updated_at = datetime.datetime.today().strftime('%Y-%m-%dT%H:%M:%S.%f')
-
+        self.updated_at = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+    
     def to_dict(self):
         dict_class = self.__dict__
         dict_class['__class__'] = self.__class__.__name__
         return dict_class
-    
+
     def __str__(self):
-        return ('[{}] {} {}'.format(__class__.__name__, self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__ ))
+    
