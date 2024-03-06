85da25a3540cfd3e4400a6fcc332907e3f6d69e1
bg-miles <piuschukwuebuka40@gmail>
2024-03-06 15:38:12 +0100

    updated the __init__ method

--- models/base_model.py
+++ models/base_model.py
@@ -1,33 +1,36 @@
 import uuid
 import datetime
 
+
 class BaseModel:
 
-	def __init__(self, *arg, **kwargs):
-		if kwargs:
-			for attr, value in kwargs.items():
-				if attr != '__class__':
-					if attr in ['created_at', 'updated_at']:
-						date_string = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
-						parse_date = datetime.datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S.%f' )
-						setattr(self,attr, parse_date)
-					else:
-						setattr(self, attr, value)
-		else:
-			self.id = str(uuid.uuid4())
-			date = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f") 
-			self.created_at =  datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f')
-			self.updated_at = self.created_at
-			
+    def __init__(self, *arg, **kwargs):
+        if kwargs is not None and kwargs != {}:
+            for attr in kwargs:
+                if attr == "created_at":
+                    self.__dict__["created_at"] = datetime.datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
+                    )
+                elif attr == "updated_at":
+                    self.__dict__["updated_at"] = datetime.datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
+                    )
+                else:
+                    self.__dict__[attr] = kwargs[attr]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
-	def save(self):
-		self.updated_at = datetime.datetime.now()
-	
-	def to_dict(self):
-		dict_class = self.__dict__
-		dict_class['__class__'] = self.__class__.__name__
-		return dict_class
+    def save(self):
+        self.updated_at = datetime.datetime.now()
 
-	def __str__(self):
-		return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__ ))
-	
+    def to_dict(self):
+        dict_class = self.__dict__
+        dict_class["__class__"] = self.__class__.__name__
+        dict_class["created_at"] = dict_class["created_at"].isoformat()
+        dict_class["updated_at"] = dict_class["updated_at"].isoformat()
+        return dict_class
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
